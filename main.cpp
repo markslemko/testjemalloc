b56c9730520c3ca463dfd036eade857820c08f17
Mark Slemko <markslemko@gmail.com>
2020-04-13 20:50:55 -0700

    large allocation testing

--- main.cpp
+++ main.cpp
@@ -30,6 +30,29 @@ int main() {
     std::cout << "done\n";
 
 
+    // test really huge allocations
+    char* largeArray = new char[1024 * 1024 * 1024];
+    char* largeArray2 = new char[1024 * 1024 * 1024];
+    char* largeArray3 = new char[1024 * 1024 * 1024];
+    char* largeArray4 = new char[1024 * 1024 * 1024];
+    
+    largeArray[1000] = 120;
+    largeArray[1000000] = 121;
+    largeArray[1000000000] = 122;
+    largeArray2[1000000000] = 123;
+    
+    printf("%zx %d\n", largeArray[999], largeArray[1000]);
+    printf("%zx %d\n", largeArray[999999], largeArray[1000000]);
+    printf("%zx %d\n", largeArray[999999999], largeArray[1000000000]);
+    printf("%zx %d\n", largeArray2[999999999], largeArray2[1000000000]);
+    
+    delete[] largeArray;
+    delete[] largeArray2;
+    delete[] largeArray3;
+    delete[] largeArray4;
+    
+    printf("done\n");
+
     coherent_rpmalloc::rpmalloc_finalize();
     return 0;
 }
